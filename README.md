5d8fad92c9f474088a7bdde6221fac8638c2d56b
sihamouda <anis.c.hamouda@gmail.com>
2022-08-04 22:41:41 +0100

    cleaner presentation

--- README.md
+++ README.md
@@ -1,25 +1,62 @@
 # ELK-STACK
 This Readme file will help you to use properly my ELK-STACK project
 
-# Initial setup
+# Getting Started
 
-1.run the following command to start the project
+To start the project, run the following command 
 
 ```bash
 docker compose up
 ```
+You will notice that ELK is not secured 
 
-2.now you need to reset passwords for elastic, kibana and logstash using the following commands 
+## Minimal Security for ELK
 
-```bash
-docker compose exec elasticsearch bin/elasticsearch-reset-password --batch --user elastic
-```
+To enable minimal security features, take a look on elasticsearch.yml kibana.yml and logstash.yml and restart the project
 
-```bash
-docker compose exec elasticsearch bin/elasticsearch-reset-password --batch --user kibana_system
-```
+* Run the following command to automatically generate passwords for built in users (**read the next option before executing**)
 
-```bash
-docker compose exec elasticsearch bin/elasticsearch-reset-password --batch --user logstash_system
-```
-3.paste the result in the .env
+    ```bash
+    docker compose exec -it elasticsearch bash bin/elasticsearch-setup-passwords auto -v --batch
+    ```
+
+* If you want to chose the passwords for built in users, run the following command
+
+    ```bash
+    docker compose exec -it elasticsearch bash bin/elasticsearch-setup-passwords interactive
+    ```
+
+* If you need to reset passwords for elastic, kibana and logstash, use the following commands 
+
+    ```bash
+    docker compose exec elasticsearch bin/elasticsearch-reset-password --batch --user elastic
+    ```
+
+    ```bash
+    docker compose exec elasticsearch bin/elasticsearch-reset-password --batch --user kibana_system
+    ```
+
+    ```bash
+    docker compose exec elasticsearch bin/elasticsearch-reset-password --batch --user logstash_system
+    ```
+    **Don't forget to paste the result in the .env**
+
+## Minimal Security for ELK + HTTPS
+
+To get a higher level of security, you need to activate the **HTTPS** on ELK
+
+1. Generate a **CA** certificate using this command (**optional**)
+
+2. Generate Certification using the **CA** certificate
+
+3. Copy certifactes to the **config** directory
+
+4. Take a look on elasticsearch.yml kibana.yml and logstash.yml and restart the project
+
+    **CHECK SETUP.SH**
+
+# Coming Features
+
+1. Interactive cli to make for faster setup
+
+2. Beats
--- setup/setup.sh
+++ setup/setup.sh
@@ -2,87 +2,91 @@ set -e
 
 setup_passwords()
 {
-    printf "######## Setting up Default User Passwords ########\n"
-    printf "##################################################\n"
+    echo "====== Setting up Default User Passwords ======"
+    echo "==============================================="
     
     bin/elasticsearch-setup-passwords auto -u "https://0.0.0.0:9200" -v --batch >> kEy
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
 reset_passwords()
 {
-    printf "######## Resetting Default User Passwords ########\n"
-    printf "##################################################\n"
+    echo "====== Resetting Default User Passwords ======"
+    echo "=============================================="
 
     bin/elasticsearch-reset-password --batch --user elastic  > kEy
     bin/elasticsearch-reset-password --batch --user kibana_system  >>kEy 
     bin/elasticsearch-reset-password --batch --user logstash_system  >>kEy 
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
 generate_ca(){
-    printf "################## Generating CA ##################\n"
-    printf "##################################################\n"
+    echo "================ Generating CA ================"
+    echo "==============================================="
     cd $ELASTIC_DIR
     bin/elasticsearch-certutil ca --pem --out /tmp/certs/elk-ca.zip
     cd /tmp/certs
     unzip elk-ca.zip
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
 generate_cert(){
-    printf "################ Generating CERTS ################\n"
-    printf "##################################################\n"
+    echo "==============  Generating CERTS ============== "
+    echo "==============================================="
     cd $ELASTIC_DIR
     bin/elasticsearch-certutil cert --ca-cert /tmp/certs/ca/ca.crt --ca-key /tmp/certs/ca/ca.key --pem --in ./config/instances.yml --out  /tmp/certs/certs.zip 
     cd /tmp/certs
     unzip certs.zip
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
 elastic_cert_cp(){
-    printf "######### Configuring Elasticsearch CERTS #########\n"
-    printf "##################################################\n"
+    echo "======= Configuring Elasticsearch CERTS ======="
+    echo "==============================================="
     
     cd $ELASTIC_DIR
     mkdir config/certs
     cp /tmp/certs/ca/ca.crt /tmp/certs/elasticsearch/* config/certs
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
 kibana_cert_cp(){
-    printf "############ Configuring Kibana CERTS #############\n"
-    printf "##################################################\n"
+    echo "========== Configuring Kibana CERTS ==========="
+    echo "==============================================="
     
     cd $KIBANA_DIR
     mkdir config/certs
     cp /tmp/certs/ca/ca.crt /tmp/certs/kibana/* config/certs
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
 logstash_cert_cp(){
-    printf "########### Configuring Logstash CERTS ###########\n"
-    printf "##################################################\n"
+    echo "=========  Configuring Logstash CERTS ========="
+    echo "==============================================="
     
     cd $LOGSTASH_DIR
     mkdir config/certs
     cp /tmp/certs/ca/ca.crt /tmp/certs/logstash/* config/certs
 
-    printf "##################### DONE!!! #####################\n"
-    printf "##################################################\n"
+    echo "=================== DONE!!! ==================="
+    echo "==============================================="
 }
 
-
-logstash_cert_cp
+# echo"=============== Welcome ELK CLI ==============="
+# echo"==============================================="
+# function="Set_Passwords Reset_Passwords Generate_CA Generate_Certs Copy_Elastic_Certs Copy_Kibana_Certs Copy_Logstash_Certs"
+# select option in $function; do
+#     echo $option
+# done
